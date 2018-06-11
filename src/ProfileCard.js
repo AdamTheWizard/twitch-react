d98ddde03b0d75eb650d3f5503d514c7a5ec11e3
Adam <mr_adam_man@hotmail.com>
2018-06-11 12:30:16 +0100

    Update ProfileCard.js

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -4,7 +4,7 @@ export class ProfileCard extends React.Component {
     render(){
         return (
             <div className="profileCardContainer">
-                
+                {this.props.firstname}
             </div>
         )
     }
